6e937a7b7f73ff6be516459d2342bc25ad1fc547
Adanna305 <52667382+Adanna305@users.noreply.github.com>
2019-07-18 11:32:32 -0400

    Add files via upload

---
+++ day9/morningExercise.java
@@ -0,0 +1,30 @@
+package day9;
+
+import java.util.Scanner;
+
+public class morningExercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	Scanner killer = new Scanner (System.in);
+	System.out.println("Welcome to the Warm Up");	
+	System.out.println("Select a number");
+		int dog = killer.nextInt();
+			
+		for ( int x = dog; x <=100; x++ ) {
+			  System.out.println(); 
+			  	 
+			   if (dog > 50)  {
+					 System.out.println(x);
+					
+		 
+		}
+		 }
+}
+		
+		  } 
+	
+
+
